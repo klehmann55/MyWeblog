a6a78bd02c9e0bb5ecf62b8605783b1ad78b9267
Kevin Lehmann <58291919+klehmann55@users.noreply.github.com>
2019-11-29 08:58:59 +0100

    modal closearea
    close the forms by clicking in the modal background

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,55 +1,27 @@
-function initBtn() {
+document.addEventListener('DOMContentLoaded', start);
+
+function start() {
 	
-	var btn = document.querySelector('#loginform button[type="submit"]');
-	btn.addEventListener('click', chk);
-}
-
-function initLBtn() {
-	var logoutBtn = document.getElementById('logout');
-	logoutBtn.addEventListener('click', removeCookie);
-}
-
-function chk() {
-	if ( document.getElementById("remember").checked ) {
-		if (document.cookie.indexOf('username=') == -1) {
-			setCookie(); 
-		}
-	} 
-	else {
-		removeCookie(); 
-	}
-}
-
-
-
-function setCookie() {
-	document.cookie = 'username=' 
-	+ document.getElementById('luname').value 
-	+ '; expires=Wed, 18 Dec 2019 12:00:00 UTC';
-	document.cookie = 'Lieblingsfarbe=Blau; expires=Wed, 18 Dec 2019 12:00:00 UTC;'; 				
-}
-
-function removeCookie() {
-	document.cookie = 'username=; expires=Thu, 01 Jan 1970 00:00:00 UTC';
-	document.cookie = 'Lieblingsfarbe=; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
-}
-
-function init() {
-	// cookie da oder nicht?
-	// if ( window.location == 'aktuelles.php' ) {
-	if ( document.cookie.indexOf('username=') > -1 ) {
-		var win1 = window.location;
-		if ( win1 == 'http://localhost/0/dwg/10_PDORegisterLogin_self/' || win1 == 'http://localhost/0/dwg/10_PDORegisterLogin_self/index.php' ) {
-			window.location = 'aktuelles.php';
-			initLBtn();
-		}
-		initLBtn();
-	}
-	else {
-		initBtn();
-	}
-}
-
-document.addEventListener('DOMContentLoaded', init);
-
+	// Get the forms
+	var rform = document.getElementById('registerform');
+	var lform = document.getElementById('loginform');
+	var fform = document.getElementById('forgotform');
+	// Get the modal
+	var modal = document.getElementById('modal');
 	
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == rform) {
+			rform.style.display = "none";
+			modal.style.display = "none";
+		}
+		if (event.target == lform) {
+			lform.style.display = "none";
+			modal.style.display = "none";
+		}
+		if (event.target == fform) {
+			fform.style.display = "none";
+			modal.style.display = "none";
+		}
+	}
+}
